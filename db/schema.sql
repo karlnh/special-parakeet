92a71c2814394cc3080dab62907760545366e378
karlnh <hubigkarl@gmail.com>
2023-01-26 20:33:30 -0600

    fix schema to create tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,28 @@
-DROP    DATABASE IF EXISTS company_db;
-CREATE  DATABASE company_db;
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
 
 USE company_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    dep_name VARCHAR(30) NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dep_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     dep_id INT,
     FOREIGN KEY (dep_id)
-    REFERENCES department(id)
+    REFERENCES department (id)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    REFERENCES role (id)
 );
