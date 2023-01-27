5b7275f7180fd519ad3dd42463af0fe84fe72a10
karlnh <hubigkarl@gmail.com>
2023-01-26 23:56:16 -0600

    add init(), adding() and viewing() funcs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
-const { mainMenuPrompt, employeesByManager, employeesByDepartment } = require('./lib/prompts');
+const prompts = require('./lib/prompts');
 
 const db = mysql.createConnection({
     host: 'localhost',
@@ -12,40 +12,162 @@ const db = mysql.createConnection({
     console.log(`Connected to company_db database.`)
 );
 
-function ask() {
-    inquirer.prompt(mainMenuPrompt).then((answers) => {
-        switch (answers.menu) {
+function viewing() {
+    inquirer.prompt([
+        {
+            name: 'viewSelect',
+            type: 'list',
+            loop: false,
+            message: "What would you like to view?",
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+            ],
+        }
+    ])
+    .then((answer) => {
+        switch (answer.viewSelect) {
             case 'View all departments':
                 db.query('SELECT * FROM department', (err, results) => {
-                    console.log('\n');
-                    console.table(results);
+                    err
+                    ? console.log(err)
+                    : console.table(results);
                 });
                 break;
             case 'View all roles':
                 db.query('SELECT * FROM role', (err, results) => {
-                    console.log('\n');
-                    console.table(results);
+                    err
+                    ? console.log(err)
+                    : console.table(results);
                 });
-                break;
             case 'View all employees':
                 db.query('SELECT * FROM employee', (err, results) => {
-                    console.log('\n');
-                    console.table(results);
+                    err
+                    ? console.log(err)
+                    : console.table(results);
                 });
                 break;
-            case 'View employees by manager ID':
-                inquirer.prompt(employeesByManager).then((answer) => {
-                    const response = Number(answer.managerID);
-                    db.query('SELECT * FROM employee WHERE manager_id = ?', response, (err, results) => {
-                        console.log('\n');
-                        console.table(results);
-                    });
-                });
             default:
-                console.log("Epic fail.");
-        };
-        ask();
+                break;
+        }
+    })
+    .then(init());
+};
+
+function adding() {
+    inquirer.prompt([
+        {
+            name: 'viewAdd',
+            type: 'select',
+            loop: false,
+            message: 'What would you like to add?',
+            choices: [
+                'Department',
+                'Role',
+                'Employee',
+            ],
+        }
+    ])
+    .then((answer) => {
+        switch (answer.viewAdd) {
+            case 'Department':
+                inquirer.prompt([
+                    {
+                        name: 'depAdd',
+                        type: 'input',
+                        message: 'Please enter the new department name.',
+                    }
+                ]).then((response) => {
+                    db.query('INSERT INTO department (dep_name) VALUES (?)', response.depAdd);
+                });
+                break;
+            case 'Role':
+                inquirer.prompt([
+                    {
+                        name: 'roleTitle',
+                        type: 'input',
+                        message: 'Please enter a role title.',
+                    },
+                    {
+                        name: 'roleSalary',
+                        type: 'input',
+                        message: 'Please enter a role salary.',
+                    },
+                    {
+                        name: 'roleDep',
+                        type: 'input',
+                        message: 'Please enter a role department ID.',
+                    },
+                ])
+                .then((response) => {
+                    db.query('INSERT INTO role (title, salary, dep_id) VALUES (?, ?, ?)', response.roleTitle, response.roleSalary, response.roleDep);
+                });
+                break;
+            case 'Employee':
+                inquirer.prompt([
+                    {
+                        name: 'firstName',
+                        type: 'input',
+                        message: 'Please enter employee first name.',
+                    },
+                    {
+                        name: 'lastName',
+                        type: 'input',
+                        message: 'Please enter employee last name.',
+                    },
+                    {
+                        name: 'roleId',
+                        type: 'input',
+                        message: 'Please enter employee role ID.',
+                    },
+                    {
+                        name: 'managerId',
+                        type: 'input',
+                        message: 'Please enter employee manager ID. If none, put null.',
+                    },
+                ])
+                .then((answers) => {
+                    db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', answers.firstName, answers.lastName, answers.roleId, answers.managerId);
+                });
+                break;
+            default:
+                break;
+        }
+        init();
+    });
+};
+
+function deleting() {
+    
+};
+
+function budgeting() {
+    
+};
+
+function init() {
+    inquirer.prompt(prompts.mainMenuPrompt).then((answers) => {
+        switch (answers.main) {
+            case 'View department, role, or employee':
+                viewing();
+                break;
+            case 'Add department, role, or employee':
+                adding();
+                break;
+            case 'Update employee':
+                updating();
+                break;
+            case 'Delete department, role, or employee':
+                deleting();
+                break;
+            case 'Calculate department budget':
+                budgeting();
+                break;
+            default:
+                break;
+        }
     });
 }
 
-ask();
+init();
